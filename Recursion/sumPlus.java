d10879e5157149efd118cac576cbdce41c4c471c
nowfutureland <126860322+nowfutureland@users.noreply.github.com>
2024-06-13 10:19:53 +0900

    Create sumPlus.java

---
+++ Recursion/sumPlus.java
@@ -0,0 +1,16 @@
+public class Recursion1_SumPlus {
+
+	static int sumPlus(int num) {
+		System.out.println("ing : "+num);
+		if(num == 1) {
+			return 1;
+		}else {
+			return num*sumPlus(num-1);
+		}
+	}
+
+public static void main(String[] args) {
+	System.out.println("Sum : " + sumPlus(10));
+}
+
+}
